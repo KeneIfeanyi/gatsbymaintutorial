b69b67a1dc97641d60b4b7a38fc92a3967c4122c
kene-Anyika ifeanyi <keneanyikaifeanyi@gmail.com>
2023-04-19 12:30:17 +0100

    new commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
     title: `Tutorial Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag"]
+  plugins: ["gatsby-plugin-google-gtag", 'gatsby-plugin-image', 'gatsby-plugin-sharp',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'blog',
+      path: `${__dirname}/blog`,
+    } 
+  }
+]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Layout from '../components/layout';
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Homepage = ()=> {
   return(
     <div>
       <Layout pageHeading='Home Page' pageTitle='Home'>
           Welcome to my HomePage
+          <StaticImage src='https://imageio.forbes.com/specials-images/imageserve/5f962df3991e5636a2f68758/0x0.jpg?format=jpg&crop=812,457,x89,y103,safe&width=1200' 
+          layout='fullWidth'/>
       </Layout>
     </div>
   )
